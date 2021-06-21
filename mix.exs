1be74c2a686daee1b846c05ef1decda975164a08
Michał Śledź <michal.sledz@swmansion.com>
2021-06-21 15:22:51 +0200

    bump to 0.5.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.Element.FLACParser.MixProject do
   use Mix.Project
 
-  @version "0.4.0"
+  @version "0.5.0"
   @github_url "https://github.com/membraneframework/membrane-element-flac-parser"
 
   def project do
@@ -63,7 +63,7 @@ defmodule Membrane.Element.FLACParser.MixProject do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.6.0"},
+      {:membrane_core, "~> 0.7.0"},
       {:membrane_caps_audio_flac, "~> 0.1.1"},
       {:crc, "~> 0.10.1"},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
