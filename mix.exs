aea5000d810f61add1bc2a17b3be7a3c4e2e8b92
Daniel Jodłoś <daniel.jodlos@swmansion.com>
2022-05-13 14:53:39 +0200

    Fix license

--- mix.exs
+++ mix.exs
@@ -55,7 +55,7 @@ defmodule Membrane.FLAC.Plugin.MixProject do
   defp package do
     [
       maintainers: ["Membrane Team"],
-      licenses: ["Apache 2.0"],
+      licenses: ["Apache-2.0"],
       links: %{
         "GitHub" => @github_url,
         "Membrane Framework Homepage" => "https://membraneframework.org"
