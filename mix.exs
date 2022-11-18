0cda5a1f2abc6742b87b00b4b5676692fda26538
Radosław Szuma <radoslawszuma@gmail.com>
2022-11-18 13:12:10 +0100

    Update core to 0.11.0

--- mix.exs
+++ mix.exs
@@ -65,13 +65,13 @@ defmodule Membrane.FLAC.Plugin.MixProject do
 
   defp deps do
     [
-      {:membrane_core, "~> 0.10.0"},
+      {:membrane_core, "~> 0.11.0"},
       {:membrane_caps_audio_flac, "~> 0.1.1"},
       {:crc, "~> 0.10.4"},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
-      {:membrane_file_plugin, "~> 0.12.0", only: :test}
+      {:membrane_file_plugin, "~> 0.13.0", only: :test}
     ]
   end
 
