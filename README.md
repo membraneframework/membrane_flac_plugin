3c8c7a6dea957c5173b53f0ecab06ca810070d65
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2023-11-09 17:17:03 +0100

    Adjust to core 1.0

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ The package can be installed by adding `membrane_flac_plugin` to your list of de
 ```elixir
 def deps do
   [
-	{:membrane_flac_plugin, "~> 0.11.0"}
+    {:membrane_flac_plugin, "~> 0.11.0"}
   ]
 end
 ```
--- mix.exs
+++ mix.exs
@@ -71,7 +71,7 @@ defmodule Membrane.FLAC.Plugin.MixProject do
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:credo, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
-      {:membrane_file_plugin, "~> 0.13.0", only: :test}
+      {:membrane_file_plugin, "~> 0.16.0", only: :test}
     ]
   end
 
