68ba29452d69da659a1fa9043bc63d86d80de4c5
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-07 17:10:04 +0200

    Relax constraints of support dependencies

--- mix.exs
+++ mix.exs
@@ -68,17 +68,16 @@ defmodule Membrane.FLAC.Plugin.MixProject do
       {:membrane_core, "~> 1.0"},
       {:membrane_flac_format, "~> 0.2.0"},
       {:crc, "~> 0.10.4"},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
       {:membrane_file_plugin, "~> 0.16.0", only: :test}
     ]
   end
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling],
-      plt_add_apps: [:syntax_tools]
+      flags: [:error_handling]
     ]
 
     if System.get_env("CI") == "true" do
